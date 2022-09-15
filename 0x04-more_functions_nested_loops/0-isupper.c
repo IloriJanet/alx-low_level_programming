2caf8221df4875e1e2fb29f1ec28e715e0150d30
IloriJanet <ilorifaith111@gmail.com>
2022-09-15 11:49:11 -0700

    It is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
+#include"main.h"
+
 /**
  * _isupper - check if c is upper
  *
  * @c: input for alphabet
  *
- * Return: 1 if its uppercase, 0 if not
+ * Return: 1 if it is uppercase, 0 if not
  */
 
-
-int_isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
